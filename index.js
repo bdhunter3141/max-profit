a857e3441c5b3f6466c6da941b133b621a2443c8
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-10 18:16:47 +0000

    Added pseudo code

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var maxProfit = function(arr) {
-
-
+//create counter variable = 0
+//loop (outer one) will start at arr[counter]
+  //compare arr[counter + 1] minus arr[counter] and return result if it's a positive number
+  //create variable to store result
+  //replace variable if arr[2] minus arr[0] is higher number
+  //continue loop
+  //push result to new array
+  // move counter for outer loop += 1
+  //arr[counter + 1] - arr [counter], etc.
+//sort result array and return newArr[0]
 }
 
 module.exports = maxProfit;
