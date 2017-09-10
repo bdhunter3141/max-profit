aa9a8f6ea0a83da8549f157b96cec31fd4863ee1
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-10 19:04:58 +0000

    Function will return an unsorted array of possible profits

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var maxProfit = function(arr) {
-//create counter variable = 0
-//loop (outer one) will start at arr[counter]
-  //compare arr[counter + 1] minus arr[counter] and return result if it's a positive number
-  //create variable to store result
-  //replace variable if arr[2] minus arr[0] is higher number
-  //continue loop
-  //push result to new array
-  // move counter for outer loop += 1
-  //arr[counter + 1] - arr [counter], etc.
-//sort result array and return newArr[0]
+  var resultArr = [];
+  for (var firstComparisonNum in arr) {
+    for (var i = firstComparisonNum; i < arr.length; i++) {
+      // console.log(arr[firstComparisonNum], arr[i])
+      if (arr[firstComparisonNum] < arr[i]) {
+        resultArr.push(arr[i] - arr[firstComparisonNum])
+      }
+    }
+  }
+  console.log(resultArr)
+
+    //compare arr[counter + 1] minus arr[counter] and return result if it's a positive number
+    //create variable to store result
+    //replace variable if arr[2] minus arr[0] is higher number
+    //continue loop
+    //push result to new array
+    // move counter for outer loop += 1
+    //arr[counter + 1] - arr [counter], etc.
+  //sort result array and return newArr[0]
+  //if newArr[0] < 1 then return -1
 }
+
+maxProfit([45, 24, 35, 31, 40, 38, 11]);
 
 module.exports = maxProfit;
