df0c379b2bf034412d26f2bc78c955a656c2beb7
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-10 19:17:22 +0000

    Updated test and finished the maxProfit function so that it passes all tests

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ var maxProfit = function(arr) {
   var resultArr = [];
   for (var firstComparisonNum in arr) {
     for (var i = firstComparisonNum; i < arr.length; i++) {
-      // console.log(arr[firstComparisonNum], arr[i])
       if (arr[firstComparisonNum] < arr[i]) {
-        resultArr.push(arr[i] - arr[firstComparisonNum])
+        resultArr.push(arr[i] - arr[firstComparisonNum]);
       }
     }
   }
-  console.log(resultArr)
-
-    //compare arr[counter + 1] minus arr[counter] and return result if it's a positive number
-    //create variable to store result
-    //replace variable if arr[2] minus arr[0] is higher number
-    //continue loop
-    //push result to new array
-    // move counter for outer loop += 1
-    //arr[counter + 1] - arr [counter], etc.
-  //sort result array and return newArr[0]
-  //if newArr[0] < 1 then return -1
+  if (resultArr[0]) {
+    resultArr.sort(function(a, b) {
+      return b - a;
+    })
+    return resultArr[0];
+  }
+    return -1;
 }
-
-maxProfit([45, 24, 35, 31, 40, 38, 11]);
 
 module.exports = maxProfit;
--- test/max-profit-test.js
+++ test/max-profit-test.js
@@ -8,7 +8,7 @@ describe("Max Profit", function() {
     assert.strictEqual(check(array), 16, 'these numbers are strictly equal')
   })
   it("Should return '-1' value if no possible profit", function() {
-    var array = [45, 24, 33, 31, 29, 24, 11]
+    var array = [45, 44, 33, 31, 29, 24, 11]
     assert.strictEqual(check(array), -1, 'these numbers are strictly equal')
   })
 })
